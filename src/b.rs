bc470436378c1983ffe6da98579a5da604714c1f
kaushik raj <algorithmunloack@gmail.com>
2024-02-05 21:55:20 +0530

    gyan

--- src/b.rs
+++ src/b.rs
@@ -1,4 +1,4 @@
 pub fn my(){
     println!("hi") ;
-    println!("This is first code succesfully connecting the remote tunnel to the github ")
+    println!("This is first code succesfully connecting the remote tunnel to the github as after 3days ")
  }
